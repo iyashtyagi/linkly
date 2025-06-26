13df01a47a1d5b595f585d9d0eb23da4153ec96d
Yash Tyagi <contactyashtyagi@gmail.com>
2025-06-26 21:17:49 +0530

    Add cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,10 +2,12 @@ import "dotenv/config";
 import express from "express";
 import router from "./routes";
 import { errorHandler } from "./middlewares";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT!;
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/", router);
